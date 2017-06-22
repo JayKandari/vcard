577651da5fcd6f703e9ba1ee92a16377ac30ebdb
Jaideep <jaideepskandari@gmail.com>
2017-06-22 23:43:07 +0530

    Added sample Formatter

---
+++ src/Plugin/Field/FieldFormatter/VcardFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\vcard\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'vcard_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "vcard_formatter",
+ *   module = "vcard",
+ *   label = @Translation("Simple vcard formatter"),
+ *   field_types = {
+ *     "text_with_summary"
+ *   }
+ * )
+ */
+class VcardFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = array();
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#markup' => '<h1>Sample Value.. :: </h1><u>' . $item->value . '</u>',
+      ];
+    }
+
+    return $elements;
+  }
+
+}
